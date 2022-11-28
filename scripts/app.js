43f314112409dc2f44e8eaace9ec59ffd1f557b0
arelymartinez2022 <armartinez@tracylc.net>
2022-11-28 10:13:57 -0800

    push

---
+++ scripts/app.js
@@ -0,0 +1,47 @@
+//alert("Welcome to JS");
+
+//how we would do it in c#
+// string firstName = "ken";
+// int value = 100;
+// bool fake = true;
+
+
+//primitive data types 
+let firstName = 'sam';
+let numOne = 35;
+let truth = true;
+
+//undifined and null
+//let undifined;
+//null is intnentionally abscense of value 
+
+
+// console.log(firstName);
+// console.log(numOne);
+
+// console.log(numOne, firstName);
+//console.log(truth);
+
+if(firstName =="ken"){
+    console.log("Hello Ken");
+} else{
+    console.log("who are you");
+}
+
+for(let i = 0; i <=10; i++){
+    console.log(i);
+}
+
+let myArray = [];
+
+myArray.push("Breakfast", "lunch", "Dinner");
+console.log(myArray);
+
+console.log(myArray[1]);
+
+let numArray = [1, 3, 5, 434, 234343, 34434];
+
+for(let i = 0; i <= numArray.length; i++){
+    console.log(numArray[i]);
+}
+
